e2364c32d1ec941cabb5a33ee5530ea7635dc35a
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2020-03-07 20:02:21 +0800

    Refactored project. Implemented React using JS Tool chain.

---
+++ src/components/app.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const App = () => <div>Start Developing!</div>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].bundle.js',
+  },
+  devServer: {
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'eslint-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
