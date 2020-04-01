3fe676512bad79113370c8624c86bfa10b38be2c
Mark Anthony Ballega Lumbao <malumbao@gmail.com>
2020-04-01 11:41:35 +0800

    Renamed build folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.join(__dirname, 'build'),
+    path: path.join(__dirname, 'dist'),
     filename: '[name].[hash].bundle.js',
     chunkFilename: '[name].[hash].bundle.js',
   },
