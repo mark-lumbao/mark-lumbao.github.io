a66813d0533ddfab66b9541a90da2a0a5008b647
Mark Lumbao <markanthonylumbao@gmail.com>
2020-04-13 09:40:38 +0800

    Added support for Routing.

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,18 +1,19 @@
 import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as ROUTES from 'constants/routes';
+import CenteredMessage from 'components/shared/centered-message';
 
-const MainNavigation = lazy(() => import('components/main-navigation'));
-const Terminal = lazy(() => import('components/shared/terminal'));
-const Footer = lazy(() => import('components/footer'));
+const Home = lazy(() => import('components/home'));
+const NotFound = lazy(() => import('components/not-found'));
 
 const App = () => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <div className="w-screen h-screen flex flex-col bg-teal-800">
-      <MainNavigation />
-      <div className="flex items-center container mx-auto flex-1 py-4 sm:px-2 md:px-2">
-        <Terminal />
-      </div>
-      <Footer />
-    </div>
+  <Suspense fallback={<CenteredMessage message="Loading Components ..." />}>
+    <Router>
+      <Switch>
+        <Route path={ROUTES.HOME} exact component={Home} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   </Suspense>
 );
 
