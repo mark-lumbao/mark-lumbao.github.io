a402f4131c9e2dd7e8faa35b33c629929ee8585a
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2020-06-11 01:49:21 +0800

    Fixed button spacing and added link to on logo click

--- src/components/main-navigation.tsx
+++ src/components/main-navigation.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from 'constants/routes';
 import logo from 'assets/images/terminal.png';
 import github from 'assets/images/github.png';
 import sun from 'assets/images/sun.png';
@@ -14,13 +16,13 @@ const isDark = (theme: string) => theme === 'theme-dark';
 const MainNavigation = ({ toggleTheme, theme }: NavProps) => (
   <nav className="border-dashed border-b-2 border-secondary p-3">
     <div className="container mx-auto flex items-center">
-      <div className="flex flex-1 items-center flex-initial">
+      <Link to={ROUTES.HOME} className="flex flex-1 items-center flex-initial">
         <img alt="logo" src={logo} className="w-8 mr-2" />
         <span className="font-bold text-base text-primaryCopy">M.A.L v0.1</span>
-      </div>
+      </Link>
       <div className="flex flex-1 justify-end">
         <button className="focus:outline-none" aria-label="Theme Switcher" type="button" onClick={() => { toggleTheme(); }}>
-          <img alt="github" src={isDark(theme) ? moon : sun} className={`${isDark(theme) ? null : 'invert'} mr-2 w-8 xsm:w-7`} />
+          <img alt="github" src={isDark(theme) ? moon : sun} className={`${isDark(theme) ? null : 'invert'} mr-4 w-8 xsm:w-7`} />
         </button>
         <a target="_blank" rel="noopener noreferrer" href="https://github.com/doldol999">
           <img alt="github" src={github} className={`${isDark(theme) ? null : 'invert'} mr-2 w-8 xsm:w-7`} />
