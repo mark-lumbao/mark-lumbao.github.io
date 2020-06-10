c74f355861d13108a66b7e483889dafad6abfe19
Mark Anthony Lumbao <markanthonylumbao@gmail.com>
2020-06-11 00:02:53 +0800

    Added theming for centered message component

--- src/components/shared/centered-message.tsx
+++ src/components/shared/centered-message.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const CenteredMessage = ({ message }: { message: string }) => (
-  <div className="h-screen flex justify-center items-center">
+  <div className="h-screen flex justify-center items-center text-primaryCopy">
     <h1>{message}</h1>
   </div>
 );
